6b6f30b7e7c4d54a4f94e41b591b99ba72616a95
Paulina100 <paulina.jedrychowska@gmail.com>
2022-11-20 19:00:48 +0100

    Plots and calculations

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,16 +1,109 @@
 # Main function
 import numpy as np
+from time import time
+import matplotlib.pyplot as plt
 
-from lufact import lufact as lu
-from invert import invert as inv
-from determinant import determinant as det
 from GenerateMatrix import generate_matrix
+from invert import invert
+from lufact import lufact as lu
+from determinant import determinant as det
 
-matrix = generate_matrix(4)
-print(matrix)
-l, u, cost = lu(matrix)
-print(l)
-print(u)
-print(l @ u)
-print(det(matrix))
-print(np.linalg.det(matrix))
+
+# Simple test for small matrix
+test_matrix = np.array([[0.92252088, 0.64621687, 0.57633497, 0.67388787],
+                        [0.4469564, 0.96570353, 0.10687379, 0.58400998],
+                        [0.67534764, 0.62991871, 0.32299196, 0.26900836],
+                        [0.32309314, 0.09388855, 0.1462494, 0.43539867]])
+
+
+print('Inversion\n', invert(test_matrix)[0], '\n')
+print('LU factorization\n')
+print('L = \n', lu(test_matrix)[0], '\n')
+print('U = \n', lu(test_matrix)[1], '\n')
+print('Determinant\n', det(test_matrix), '\n')
+
+# Calculating time and number of operations for matrix 2^k x 2^k
+
+times_invert = []
+times_lu = []
+times_det = []
+
+operations_invert = []
+operations_lu = []
+operations_det = []
+
+for k in range(7, 8):
+    size = 2 ** k
+    matrix = generate_matrix(size)
+
+    start = time()
+    operations_invert.append(invert(matrix)[1])
+    end = time()
+    times_invert.append(round(end - start, 4))
+
+    start = time()
+    operations_lu.append(lu(matrix)[2])
+    end = time()
+    times_lu.append(round(end - start, 4))
+
+    start = time()
+    operations_det.append(det(matrix)[1])
+    end = time()
+    times_det.append(round(end - start, 4))
+
+print("\nINVERSION:\n")
+print("Time:\n", times_invert)
+print("Floating point operations:\n", operations_invert)
+print("\nLU FACTORIZATION:\n")
+print("Time:\n", times_lu)
+print("Floating point operations:\n", operations_lu)
+print("\nDETERMINANT:\n")
+print("Time:\n", times_det)
+print("Floating point operations:\n", operations_det)
+
+# Overriding lists with already calculated values
+times_invert = [0.0, 0.003, 0.017, 0.1366, 0.9335]
+times_lu = [0.001, 0.002, 0.023, 0.1242, 0.7844]
+times_det = [0.0, 0.002, 0.0339, 0.1192, 0.7844]
+operations_invert = [286, 3074, 26446, 206114, 1529326]
+operations_lu = [173, 2169, 20635, 170773, 1312323]
+operations_det = [177, 2177, 20651, 170805, 1312387]
+
+
+# Plots for time and floating point operations
+
+plt.plot(range(2, len(times_invert) + 2), times_invert)
+plt.xlabel('Size of matrix [2^k x 2^k]')
+plt.ylabel('Time [s]')
+# plt.title('Wykres 1.1. Czas działania algorytmu odwracania macierzy')
+plt.show()
+
+plt.plot(range(2, len(operations_invert) + 2), operations_invert)
+plt.xlabel('Size of matrix [2^k x 2^k]')
+plt.ylabel('Floating point operations')
+# plt.title('Wykres 1.2. Ilość operacji zmiennoprzecinkowych algorytmu odwracania macierzy')
+plt.show()
+
+plt.plot(range(2, len(times_lu) + 2), times_lu)
+plt.xlabel('Size of matrix [2^k x 2^k]')
+plt.ylabel('Time [s]')
+# plt.title('Wykres 2.1. Czas działania algorytmu LU faktoryzacji')
+plt.show()
+
+plt.plot(range(2, len(operations_lu) + 2), operations_lu)
+plt.xlabel('Size of matrix [2^k x 2^k]')
+plt.ylabel('Floating point operations')
+# plt.title('Wykres 2.2. Ilość operacji zmiennoprzecinkowych algorytmu LU faktoryzacji')
+plt.show()
+
+plt.plot(range(2, len(times_lu) + 2), times_det)
+plt.xlabel('Size of matrix [2^k x 2^k]')
+plt.ylabel('Time [s]')
+# plt.title('Wykres 3.1. Czas działania algorytmu obliczania wyznacznika')
+plt.show()
+
+plt.plot(range(2, len(operations_lu) + 2), operations_det)
+plt.xlabel('Size of matrix [2^k x 2^k]')
+plt.ylabel('Floating point operations')
+# plt.title('Wykres 3.2. Ilość operacji zmiennoprzecinkowych algorytmu obliczania wyznacznika')
+plt.show()
